1509edf50115b1a17fe33147a3d1b7212ffe9aab
rokas <rokasra@gmail.com>
2022-07-20 15:07:54 +0300

    init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'airbnb-typescript'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': [2],
+    '@typescript-eslint/indent': [0],
+  },
+};
